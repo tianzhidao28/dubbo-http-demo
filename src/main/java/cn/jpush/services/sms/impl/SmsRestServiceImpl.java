4066f81d1cea708126394e15794eab84b1c732d4
rocyuan <tianzhidao4422@gmail.com>
2015-09-09 03:16:49 +0800

    dubbo--->rest service

--- src/main/java/cn/jpush/services/sms/impl/SmsRestServiceImpl.java
+++ src/main/java/cn/jpush/services/sms/impl/SmsRestServiceImpl.java
@@ -1,20 +1,27 @@
 package cn.jpush.services.sms.impl;
 
 import cn.jpush.services.sms.SmsService;
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.Date;
 
 /**
  * Created by rocyuan on 2015/9/9.
  */
-
+@Path("sms")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
 public class SmsRestServiceImpl implements SmsService {
     /**
      * @param phone
      * @param content
      */
     @Override
-    public void send(String phone, String content) {
+    @GET
+    @Path("send")
+    public void send(@FormParam("phone")String phone, @FormParam("content")String content) {
 
     }
 
